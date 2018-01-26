5b7893e6c3b946f560b0be4e2a3bcf0a71336651
Giuseppe Verni <g.verni@gmail.com>
2018-01-26 16:08:04 +0000

    Added hyperlink to cinema reward in gauge

--- src/js/modules/GradientGauge.js
+++ src/js/modules/GradientGauge.js
@@ -45,19 +45,11 @@ window.GradientGauge = function (size) {
     svgElem.setAttribute('version', '1.1')
     svgElem.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
     svgElem.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink')
-   // svgElem.setAttribute('x', '0px')
-    //svgElem.setAttribute('y', '0px')
     svgElem.setAttribute('width', this.size)
     svgElem.setAttribute('height', this.size + 10) // +10 for caption margin 
     svgElem.setAttribute('viewBox', '0 -10 ' + this.size + ' ' + this.size)
-    //svgElem.setAttribute('xml:space', 'preserve')
     svgElem.setAttribute('preserveAspectRatio','xMidYMin meet')
 
-    // var circleRatio = this.size / 200
-    // appendSVGElement(svgElem, 'g', {}, '<text id="gaugeCaption" transform="translate(6 6)" font-family="Mukta Malar"><textPath xlink:href="#path0_fill" startOffset="58%"></textPath></text><use xlink:href="#path0_fill" fill="#F7F7F9" transform="' + matrix(circleRatio, 10 * circleRatio, 10 * circleRatio) + '"/>')
-    // appendSVGElement(svgElem, 'g', {'filter': 'url(#filter0_f)'}, '<use xlink:href="#path1_fill" transform="' + matrix(circleRatio, 35 * circleRatio, 35 * circleRatio) + '" fill="#E0E0E0"/>')
-    // appendSVGElement(svgElem, 'g', {}, '<use xlink:href="#path1_fill" transform="' + matrix(circleRatio, 35 * circleRatio, 35 * circleRatio) + '" fill="#FFFFFF"/>')
-    // appendSVGElement(svgElem, 'defs', {}, '<filter id="filter0_f" filterUnits="userSpaceOnUse" x="' + 25 * circleRatio + '" y="' + 25 * circleRatio + '" width="' + 156 / 200 * this.size + '" height="' + 156 / 200 * this.size + '" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="5" result="effect1_foregroundBlur"/></filter><path id="path0_fill" d="M 186 93C 186 144.362 144.362 186 93 186C 41.6375 186 0 144.362 0 93C 0 41.6375 41.6375 0 93 0C 144.362 0 186 41.6375 186 93Z"/><path id="path1_fill" d="M 136 68C 136 105.555 105.555 136 68 136C 30.4446 136 0 105.555 0 68C 0 30.4446 30.4446 0 68 0C 105.555 0 136 30.4446 136 68Z"/></defs>')
     var circlesElem = document.createElementNS('http://www.w3.org/2000/svg', 'image')
     circlesElem.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '../images/cerchio.svg')
     circlesElem.setAttribute('height', this.size)
@@ -73,24 +65,6 @@ window.GradientGauge = function (size) {
       id: 'gaugeCaption',
       'font-family': 'Mukta Malar'
     }, '<textPath xlink:href="#pathCircle' + this.size + '" startOffset="20%"></textPath></text>')
-    // appendSVGElement(svgElem, 'path', {stroke: "black", d: `M${(this.size / 2) - captionCircleRadius},${this.size/2} a${captionCircleRadius},${captionCircleRadius} 0 0 1 ${captionCircleRadius*2},0`},'')
-    // var outerCircleElem = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-    // outerCircleElem.setAttribute('cx', this.size / 2)
-    // outerCircleElem.setAttribute('cy', this.size / 2)
-    // outerCircleElem.setAttribute('r', this.size / 2)
-    // outerCircleElem.setAttribute('fill', '#F7F7F9')
-    // svgElem.appendChild(outerCircleElem)
-    //
-    // var innerShadowGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g')
-    // var innerShadow = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-    //
-    // // Inner circle = outer circle * 0.7311
-    // var innerCircleElem = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-    // outerCircleElem.setAttribute('cx', this.size * 0.7311 / 2)
-    // outerCircleElem.setAttribute('cy', this.size * 0.7311 / 2)
-    // outerCircleElem.setAttribute('r', this.size * 0.7311 / 2)
-    // outerCircleElem.setAttribute('fill', '#FFFFFF')
-    // svgElem.appendChild(innerCircleElem)
 
     for (let i = 0; i < this.numBars; i++) {
       let lineElem = document.createElementNS('http://www.w3.org/2000/svg', 'line')
@@ -133,7 +107,11 @@ window.GradientGauge = function (size) {
       rewardIconElem.setAttribute('width', this.size)
       rewardIconElem.setAttribute('x', (this.size - rewardIconElem.getAttribute('width')) / 2)
       rewardIconElem.setAttribute('y', (this.size - rewardIconElem.getAttribute('height')) / 2)
-      svgElem.appendChild(rewardIconElem)
+      var rewardIconHref = document.createElementNS('http://www.w3.org/2000/svg', 'a')
+      rewardIconHref.setAttribute('href', 'https://member.vitality.co.uk/PartnersRewards/f/freecinematickets')
+      rewardIconHref.setAttribute('target', '_blank')
+      rewardIconHref.appendChild(rewardIconElem)
+      svgElem.appendChild(rewardIconHref)
     }
     if (animationProgress < this.points) {
       animationProgress++
